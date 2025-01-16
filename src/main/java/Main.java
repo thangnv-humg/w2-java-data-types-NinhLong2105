960648c231701cc5cf7219336c73d79b5613ed18
NinhLong2105 <anhyouem24074@gmail.com>
2025-01-16 14:54:06 +0700

    ADD FILE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
+        int a = 42;
+        double b = 3.14;
+        String c = "Hello, Java!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
-        // Ví dụ:
-        // int myInt = 42;
-        // double myDouble = 3.14;
-        // String myString = "Hello, Java!";
-        // System.out.println(myInt);
-        // System.out.println(myDouble);
-        // System.out.println(myString);
+        if (a>10){
+            System.out.println("Greater than 10");
+        }else{
+            System.out.println("Less or equal to 10");
+        }
     }
 }
+
