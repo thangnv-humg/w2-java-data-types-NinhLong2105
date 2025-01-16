a239452717c7e0c7e662b9125eece730e713ea7b
NinhLong2105 <anhyouem24074@gmail.com>
2025-01-16 14:46:52 +0700

    ADD FILE

---
+++ MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass{
+    public static void main(String[] args) {
+        int a = 42;
+        double b = 3,14;
+        string c = "Hello, Java!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c); 
+    }
+}
